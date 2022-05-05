4974659f39a99b3907b4c52f036a78ae73e8a326
mkostic21 <mislav.kostic@gmail.com>
2022-05-05 14:22:49 +0200

    Adding textView programmatically

--- app/src/main/java/com/raywenderlich/emotionalface/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/emotionalface/MainActivity.kt
@@ -31,12 +31,16 @@
 package com.raywenderlich.emotionalface
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+
+    val textView = TextView(this)
+    textView.text = "Hello Custom Views"
+    setContentView(textView)
   }
 }
